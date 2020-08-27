4601ed42d89fd722ca92759a1bf52c4e50fd3955
Saiydur Rahman <57300418+Saiydur@users.noreply.github.com>
2020-08-27 23:29:02 +0600

    Adding Shuffle With 20 Course

--- src/FakeData/FakeData.js
+++ src/FakeData/FakeData.js
@@ -139,6 +139,14 @@ var courses = [
             instructor: "Ahmed",
             price: 30.99,
       }
-]
 
+      
+]
+            const shuffle = a => {
+                  for(let i = a.length; i;i--){
+                        let j = Math.floor(Math.random() *i);
+                        [a[i-1],a[j]] = [a[j],a[i-1]];
+                  }
+            }
+            shuffle(courses);
 export default courses;
